1d3bfd219965ab3c8c3f52abaddd31cf336cc423
mosnipe_win <sniya.pagone824055@outlook.jp>
2023-10-09 18:23:14 +0900

    バリデーション対策・CSRF対策記述

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
+session_start();
+
+//CSRF対策
+
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
+    //CSRF対策
+    if (!isset($_POST['token'] || $_POST['token'] !== getToken())) {
+        exit('処理を正常に完了できませんでした');
+    }
+    
     // バリデーション
     
     $inquiry = $_POST['inquiry']; //提出された値の定義＋取り出し
@@ -17,12 +26,31 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $error['name'] = '必ずご記入ください';
     }
 
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error['email'] = 'メールアドレスの形式が正しくありません';
+    }
+
     if (empty($error)) {
         header('Location: thanks.html');//フォーム提出後のサンクスページへの遷移記述
         exit;
     }
 
 
+}
+
+/*
+ * CSRF対策用の IDを作る
+ */
+function getToken() {
+    return hash('sha256', session_id());
+}
+
+/*
+ * HTMLの特殊文字をエスケープします
+ */
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 ?>
@@ -35,14 +63,21 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 <body>
     <h1>お問い合わせフォーム</h1>
     <form action="" method="post">
+        <input type="hidden" name="token" value="<?php echo getToken(); ?>">
         <p>お問い合わせ内容 ※必須</p>
         <?php 
-            
+            if (isset($error['inquiry'])) echo h($error['inquiry']);
         ?>
         <p><textarea name="inquiry" rows="10" cols="100"></textarea></p>
         <p>お名前 ※必須</p>
+        <?php 
+            if (isset($error['name'])) echo h($error['name']);
+        ?>
         <p><input type="text" name="name" vale=""></p>
         <p>ご連絡用Email ※必須</p>
+        <?php 
+            if (isset($error['email'])) echo h($error['email']);
+        ?>
         <p><input type="email" name="email" vale=""></p>
         <p><input type="submit" value="送信"></p>
     </form>
